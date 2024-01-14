4f4b6bdfac3dd9fd8dde287e92b80443a5d8d9a7
Jakub Janaszewski (DOC) <DOC@kmd.dk>
2024-01-14 18:26:37 +0100

    Git diffrent setup aproach

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Domain.Entities;
+
+namespace Blog.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoList> TodoLists { get; }
+
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Blog.Application.Common.Interfaces;
+using Blog.Domain.Entities;
+using Blog.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
