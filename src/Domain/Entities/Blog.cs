944accee295f8a50f87e29ec2941d7d1e8f29089
Jakub Janaszewski (DOC) <DOC@kmd.dk>
2024-01-14 18:42:32 +0100

    Added basic blog entity

---
+++ src/Domain/Entities/Blog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Domain.Entities;
+public class Blog : BaseAuditableEntity
+{
+    public string Title { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
+    public string Content { get; set; } = null!;
+
+    public Guid UserId { get; set; }
+    public IdentityUser User { get; set; } = null!;
+}
