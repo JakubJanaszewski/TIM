2625cfc2fd56125ed2059909e63b932fcd584b65
Jakub Janaszewski <jakub.janaszewski@student.wat.edu.pl>
2024-01-16 22:12:28 +0100

    Getting all blogs

---
+++ src/Application/Blogs/Queries/GetBlogs/BlogDto.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Domain.Entities;
+
+namespace Blog.Application.Blogs.Queries.GetBlogs;
+public class BlogDto
+{
+    public required string Title { get; init; }
+    public required string ImageUrl { get; set; }
+    public required string Content { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<BlogPage, BlogDto>();
+        }
+    }
+}
--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -4,5 +4,7 @@ namespace Blog.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<BlogPage> Blogs { get; }
+
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
