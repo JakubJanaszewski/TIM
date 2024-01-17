5c39cba0dd3ee695e54d4acb69a73f72a4494fb6
Jakub Janaszewski <jakub.janaszewski@student.wat.edu.pl>
2024-01-17 19:49:57 +0100

    Adding and removing tag API

---
+++ src/Application/Tags/Commands/CreateTag/CreateTag.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Application.Common.Interfaces;
+using Blog.Domain.Entities;
+
+namespace Blog.Application.Tags.Commands.CreateTag;
+public record CreateTagCommand : IRequest
+{
+    public required string Name { get; init; }
+}
+
+public class CreateTagCommandHandler(IApplicationDbContext context) : IRequestHandler<CreateTagCommand>
+{
+    private readonly IApplicationDbContext _context = context;
+
+    public async Task Handle(CreateTagCommand request, CancellationToken cancellationToken)
+    {
+        _context.Tags.Add(new Tag()
+        {
+            Name = request.Name,
+        });
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Application/Tags/Commands/DeleteTag/DeleteTag.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Application.Common.Interfaces;
+using Blog.Application.Common.Security;
+using Blog.Domain.Constants;
+using Blog.Domain.Entities;
+
+namespace Blog.Application.Tags.Commands.DeleteTag;
+
+[Authorize(Roles = Roles.Administrator)]
+public record DeleteTagCommand : IRequest
+{
+    public required string Name { get; init; }
+}
+
+public class DeleteTagCommandHandler(IApplicationDbContext context) : IRequestHandler<DeleteTagCommand>
+{
+    private readonly IApplicationDbContext _context = context;
+
+    public async Task Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+    {
+        Tag? tag = _context.Tags
+            .FirstOrDefault(t => t.Name == request.Name);
+
+        Guard.Against.NotFound(request.Name, tag);
+
+        _context.Tags.Remove(tag);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
