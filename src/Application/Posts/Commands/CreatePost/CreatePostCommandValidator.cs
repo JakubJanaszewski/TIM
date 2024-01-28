40bb1767dffb053435301e038d5dbc33da6f40b9
Jakub Janaszewski <jakub.janaszewski@student.wat.edu.pl>
2024-01-28 15:50:07 +0100

    validation

---
+++ src/Application/Posts/Commands/CreatePost/CreatePostCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Application.Posts.Commands.CreatePost;
+public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+{
+    public CreatePostCommandValidator()
+    {
+        RuleFor(x => x.Title)
+            .MaximumLength(100);
+
+        RuleFor(x => x.Content)
+            .MaximumLength(500);
+    }
+}
